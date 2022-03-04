4d9dee188687f23265eb611fcf9bb1abd966c3bb
MuXiu1997 <muxiu1997@gmail.com>
2022-03-04 17:45:55 +0800

    nuclear reactor v2

--- nuclear_reactor.lua
+++ nuclear_reactor.lua
@@ -1,8 +1,8 @@
 -- Nuclear Reactor
 
-local sides = require('sides')
 local component = require('component')
-
+local computer = require('computer')
+local sides = require('sides')
 
 --region SETUP
 local SIDES = {
@@ -17,11 +17,17 @@ local ADDRESSES = {
 }
 -- coolant cell
 local C = {
-  name = 'gregtech:gt.360k_NaK_Coolantcell'
+  name       = 'gregtech:gt.360k_NaK_Coolantcell',
+  is_damaged = function(stack)
+    return stack.maxDamage - 1 <= stack.damage
+  end
 }
 -- fuel rod
 local F = {
-  name = 'gregtech:gt.reactorMOXQuad'
+  name       = 'gregtech:gt.reactorMOXQuad',
+  is_damaged = function(stack)
+    return stack.damage == stack.maxDamage
+  end
 }
 -- empty
 local E = {}
@@ -38,18 +44,9 @@ local LAYOUT = {
 local transposer = component.proxy(component.get(ADDRESSES.TRANSPOSER))
 local redstone = component.proxy(component.get(ADDRESSES.REDSTONE))
 
-function find_input(what)
-  for slot = 1, transposer.getInventorySize(SIDES.INPUT) do
-    local stack = transposer.getStackInSlot(SIDES.INPUT, slot)
-    if stack ~= nil and stack.name == what.name then
-      return slot
-    end
-  end
-  return -1
-end
-
 local reactor = {
-  started = false,
+  started       = false,
+  damaged_slots = {},
 }
 
 function reactor:initialization_check()
@@ -83,94 +80,88 @@ end
 function reactor:stop()
   if self.started then
     redstone.setOutput(SIDES.NUCLEAR_REACTOR_REDSTONE, 0)
-    os.sleep(1)
     self.started = false
   end
 end
 
-function reactor:filter(func --[[ function(slot: number): boolean ]])
-  local result = {}
-  for slot = 1, transposer.getInventorySize(SIDES.NUCLEAR_REACTOR) do
-    if func(slot) then
-      result[#result + 1] = slot
+function reactor:find_input(what)
+  while true do
+    for slot = 1, transposer.getInventorySize(SIDES.INPUT) do
+      local stack = transposer.getStackInSlot(SIDES.INPUT, slot)
+      if stack ~= nil and stack.name == what.name then
+        return slot
+      end
     end
+    -- missing items, waiting
+    os.sleep(5)
   end
-  return result
 end
 
-function reactor:discharge(slots)
-  for _, slot in ipairs(slots) do
+function reactor:discharge()
+  for _, slot in ipairs(self.damaged_slots) do
     while transposer.getSlotStackSize(SIDES.NUCLEAR_REACTOR, slot) ~= 0 do
-      transposer.transferItem(SIDES.NUCLEAR_REACTOR, SIDES.OUTPUT, 1, slot)
-      os.sleep(1)
+      -- output is blocked, waiting
+      if transposer.transferItem(SIDES.NUCLEAR_REACTOR, SIDES.OUTPUT, 1, slot) == 0 then
+        os.sleep(5)
+      end
     end
   end
 end
 
-function reactor:load(slots, what)
-  while #slots > 0 do
+function reactor:load()
+  while #self.damaged_slots > 0 do
+    local slot = self.damaged_slots[1]
+    local what = LAYOUT[slot]
     local input_slot = find_input(what)
-    if input_slot ~= -1 then
-      transposer.transferItem(SIDES.INPUT, SIDES.NUCLEAR_REACTOR, 1, input_slot, slots[1])
-      table.remove(slots, 1)
-    else
-      os.sleep(1)
-    end
+    transposer.transferItem(SIDES.INPUT, SIDES.NUCLEAR_REACTOR, 1, input_slot, slot)
+    table.remove(self.damaged_slots, 1)
   end
 end
 
-function reactor:ensure_coolant_cells()
-  local damaged_coolant_cells = self:filter(
-    function(slot)
-      if LAYOUT[slot] ~= C then
-        return false
-      end
-      local stack = transposer.getStackInSlot(SIDES.NUCLEAR_REACTOR, slot)
-      if stack.maxDamage - 1 <= stack.damage then
-        self:stop()
+function reactor:has_damaged()
+  for slot = 1, transposer.getInventorySize(SIDES.NUCLEAR_REACTOR) do
+    local stack = transposer.getStackInSlot(SIDES.NUCLEAR_REACTOR, slot)
+    if stack ~= nil then
+      if LAYOUT[slot].is_damaged(stack) then
         return true
       end
-      return false
     end
-  )
-
-  if #damaged_coolant_cells == 0 then return end
-
-  self:stop()
-  self:discharge(damaged_coolant_cells)
-  self:load(damaged_coolant_cells, C)
+  end
+  return false
 end
 
-function reactor:ensure_fuel_rods()
-  local damaged_fuel_rods = self:filter(
-    function(slot)
-      if LAYOUT[slot] ~= F then
-        return false
-      end
-      local stack = transposer.getStackInSlot(SIDES.NUCLEAR_REACTOR, slot)
-      if stack.damage == stack.maxDamage then
-        self:stop()
-        return true
-      end
-      return false
-    end
-  )
-
-  if #damaged_fuel_rods == 0 then return end
+function reactor:ensure()
+  if not self:has_damaged() then
+    return
+  end
 
   self:stop()
-  self:discharge(damaged_fuel_rods)
-  self:load(damaged_fuel_rods, F)
+  os.sleep(1)
+
+  for slot = 1, transposer.getInventorySize(SIDES.NUCLEAR_REACTOR) do
+    local stack = transposer.getStackInSlot(SIDES.NUCLEAR_REACTOR, slot)
+    if stack ~= nil then
+      if LAYOUT[slot].is_damaged(stack) then
+        table.insert(self.damaged_slots, slot)
+      end
+    end
+  end
+
+  self:discharge()
+  self:load()
 end
 
 function reactor:loop()
-  self:ensure_coolant_cells()
-  self:ensure_fuel_rods()
+  self:ensure()
   self:start()
 end
 
 function reactor:run()
   if not self:initialization_check() then
+    for _ = 1, 3 do
+      computer.beep('.')
+      os.sleep(1)
+    end
     return
   end
   while true do
@@ -179,7 +170,7 @@ function reactor:run()
   end
 end
 
-function main()
+local function main()
   reactor:run()
 end
 

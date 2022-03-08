251f4906ebc4f021e88b97c7a1c5e207ab3c9373
MuXiu1997 <muxiu1997@gmail.com>
2022-03-08 11:46:30 +0800

    update large chemical reactor

--- largeChemicalReactor.lua
+++ largeChemicalReactor.lua
@@ -1,9 +1,7 @@
 -- Large Chemical Reactor Automation
 
 local component = require('component')
-local newPlaceholder = require('placeholder')
 local sides = require('sides')
-local tu = require('transposerUtil')
 local thread = require('thread')
 
 --region SETUP
@@ -28,7 +26,19 @@ local PLACEHOLDER_PREFIX = 'LCR:'
 local inputTransposer = component.proxy(component.get(ADDRESSES.INPUT_TRANSPOSER))
 local outputTransposer = component.proxy(component.get(ADDRESSES.OUTPUT_TRANSPOSER))
 
-local placeholder = newPlaceholder(PLACEHOLDER_PREFIX)
+local placeholder = {
+  prefix       = PLACEHOLDER_PREFIX,
+  match        = function(self, str)
+    return string.sub(str, 1, string.len(self.prefix)) == self.prefix
+  end,
+  removePrefix = function(self, str)
+    return string.sub(str, string.len(self.prefix) + 1)
+  end
+}
+
+local function isInputEmpty()
+  return transposer.getSlotStackSize(SIDES.INPUT, 1) == 0
+end
 
 local function tableSize(t)
   local count = 0
@@ -44,85 +54,104 @@ local function isNonEmptyVolumetricFlask(stack)
   return stack.name == 'gregtech:gt.Volumetric_Flask' and stack.amount > 0
 end
 
+local function waitingForCompletion()
+  while true do
+    if outputTransposer.getTankLevel(SIDES.TANK_O, 1) > 0 or outputTransposer.getSlotStackSize(SIDES.LCR_OUTPUT, 1) > 0 then
+      return
+    end
+  end
+end
+
 local function loop()
-  if (tu.isEmpty(inputTransposer, SIDES.INPUT)) then
+  if isInputEmpty() then
     return
   end
 
   local fluidOutputSolution = {}
-  for slot = 1, inputTransposer.getInventorySize(SIDES.INPUT) do
-    local stack = inputTransposer.getStackInSlot(SIDES.INPUT, slot)
-    if stack == nil then
-      break
-    end
+  local inputSlot = 1
+  for stack in transposer.getAllStack(SIDES.INPUT) do
     if isNonEmptyVolumetricFlask(stack) then
-      thread.create(tu.waitTransferSlotStack, inputTransposer, SIDES.INPUT, SIDES.TANK_I, slot)
+      while true do
+        if inputTransposer.transferItem(SIDES.INPUT, SIDES.TANK_I, 64, inputSlot) == 0 then
+          break
+        end
+      end
     elseif isEmptyVolumetricFlask(stack) then
-      tu.transferSlotStack(inputTransposer, SIDES.INPUT, SIDES.BUFFER_I, slot)
-    elseif stack.label ~= nil and placeholder:match(stack.label) then
+      inputTransposer.transferItem(SIDES.INPUT, SIDES.BUFFER_I, 64, inputSlot)
+    elseif stack.label and placeholder:match(stack.label) then
       local patternId = placeholder:removePrefix(stack.label)
       fluidOutputSolution = dofile(patternId)
     else
-      tu.transferSlotStack(inputTransposer, SIDES.INPUT, SIDES.LCR_INPUT, slot)
+      inputTransposer.transferItem(SIDES.INPUT, SIDES.LCR_INPUT, 64, inputSlot)
     end
+    inputSlot = inputSlot + 1
   end
 
   local volumetricFlaskMap = {}
-  for slot = 1, outputTransposer.getInventorySize(SIDES.BUFFER_O) do
-    local stack = outputTransposer.getStackInSlot(SIDES.BUFFER_O, slot)
-    if stack == nil then
-      break
+  local bufferSlot = 1
+  for stack in transposer.getAllStack(SIDES.BUFFER_I) do
+    if stack.capacity then
+      volumetricFlaskMap[stack.capacity] = slot
     end
-    volumetricFlaskMap[stack.capacity] = slot
+    bufferSlot = bufferSlot + 1
   end
 
-  while true do
-    if outputTransposer.getTankLevel(SIDES.TANK_O, 1) > 0 or outputTransposer.getSlotStackSize(SIDES.LCR_OUTPUT, 1) > 0 then
-      break
+  waitingForCompletion()
+
+  local emptyingTank = thread.create(function()
+    while tableSize(fluidOutputSolution) > 0 do
+      local fluidName = ''
+      while true do
+        local fluid = outputTransposer.getFluidInTank(SIDES.TANK_O, 1)
+        if fluid.amount > 0 then
+          fluidName = fluid.name
+          break
+        end
+      end
+
+      local solution = fluidOutputSolution[fluidName]
+
+      local remainingAmount = 0
+      for capacity, size in pairs(solution) do
+        remainingAmount = remainingAmount + (capacity * size)
+      end
+
+      for capacity, size in pairs(solution) do
+        for _ = 1, size do
+          while true do
+            if remainingAmount > capacity and outputTransposer.getTankLevel(SIDES.TANK_O, 1) > capacity then
+              break
+            end
+            if remainingAmount == capacity and outputTransposer.getTankLevel(SIDES.TANK_O, 1) == capacity then
+              break
+            end
+          end
+          while true do
+            if outputTransposer.transferItem(SIDES.BUFFER_O, SIDES.TANK_O, 1, volumetricFlaskMap[capacity]) == 1 then
+              break
+            end
+          end
+          remainingAmount = remainingAmount - capacity
+        end
+      end
+      fluidOutputSolution[fluidName] = nil
     end
-  end
+  end)
 
-  local emptyingTank = thread.create(
-    function()
-      while tableSize(fluidOutputSolution) > 0 do
-        local fluidName = ''
-        while true do
-          local fluid = outputTransposer.getFluidInTank(SIDES.TANK_O, 1)
-          if fluid.amount > 0 then
-            fluidName = fluid.name
-            break
-          end
-        end
-
-        local solution = fluidOutputSolution[fluidName]
-
-        local remainingAmount = 0
-        for capacity, size in pairs(solution) do
-          remainingAmount = remainingAmount + (capacity * size)
-        end
-
-        for capacity, size in pairs(solution) do
-          for _ = 1, size do
-            while true do
-              if remainingAmount > capacity and outputTransposer.getTankLevel(SIDES.TANK_O, 1) > capacity then
-                break
-              end
-              if remainingAmount == capacity and outputTransposer.getTankLevel(SIDES.TANK_O, 1) == capacity then
-                break
-              end
-            end
-            while true do
-              if outputTransposer.transferItem(SIDES.BUFFER_O, SIDES.TANK_O, 1, volumetricFlaskMap[capacity]) == 1 then break end
-            end
-            remainingAmount = remainingAmount - capacity
-          end
-        end
-        fluidOutputSolution[fluidName] = nil
+  local emptyingLcrOutput = thread.create(function()
+    while true do
+      if outputTransposer.transferItem(SIDES.LCR_OUTPUT, SIDES.OUTPUT_O) == 0 then
+        return
       end
     end
-  )
-  local emptyingLcrOutput = thread.create(tu.transferAllSlotStacks, outputTransposer, SIDES.LCR_OUTPUT, SIDES.OUTPUT_O)
-  local emptyingLcrInput = thread.create(tu.transferAllSlotStacks, inputTransposer, SIDES.LCR_INPUT, SIDES.OUTPUT_I)
+  end)
+  local emptyingLcrInput = thread.create(function()
+    while true do
+      if inputTransposer.transferItem(SIDES.LCR_INPUT, SIDES.OUTPUT_I) == 0 then
+        return
+      end
+    end
+  end)
 
   thread.waitForAll({ emptyingTank, emptyingLcrOutput, emptyingLcrInput })
   inputTransposer.transferItem(SIDES.INPUT, SIDES.OUTPUT_I)
